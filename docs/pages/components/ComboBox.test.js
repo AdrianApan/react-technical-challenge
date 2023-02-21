f335d8e64122aef09ee4f6c42c01a7f817da40f5
Adrian Apan <adrian.apan@getground.co.uk>
2023-02-21 14:34:40 +0000

    ✅ update combobox tests

--- docs/pages/components/ComboBox.test.js
+++ docs/pages/components/ComboBox.test.js
@@ -1,40 +1,58 @@
 import * as React from 'react';
 import { expect } from 'chai';
 import { createClientRender, fireEvent, screen } from 'test/utils';
-import { spy } from 'sinon';
 import ComboBox from './ComboBox';
 
+const MOCK_OPTIONS = ['foo', 'bar'];
 /**
  * You can run these tests with `yarn t ComboBox`.
  */
 describe('<ComboBox />', () => {
   const render = createClientRender();
 
-  it('should prevent the default event handlers', () => {
-    const handleSubmit = spy();
-    const handleChange = spy();
-    const { getAllByRole } = render(
-      <div
-        onKeyDown={(event) => {
-          if (!event.defaultPrevented && event.key === 'Enter') {
-            handleSubmit();
-          }
-        }}
-      >
-        {/* The ComboBox component here */}
-        <ComboBox onChange={handleChange} />
-      </div>,
-    );
+  it('should accept any input value', () => {
+    render(<ComboBox options={MOCK_OPTIONS} />);
+    const textbox = screen.getByRole('combobox');
+    textbox.focus();
+    fireEvent.change(textbox, { target: { value: 'foo' } });
+    expect(textbox).to.have.attribute('value', 'foo');
+  });
 
-    const textbox = screen.getByRole('textbox');
+  it('should open the suggestions list on user input', () => {
+    const { getByRole } = render(<ComboBox options={MOCK_OPTIONS} />);
+    const textbox = getByRole('combobox');
+    textbox.focus();
+    fireEvent.change(textbox, { target: { value: 'foo' } });
+    const listbox = getByRole('listbox');
+    expect(listbox).to.have.attribute('id', 'countries');
+  });
 
+  it("should show 'no suggestion' message on no matching suggestion", () => {
+    const { getByRole } = render(<ComboBox options={MOCK_OPTIONS} />);
+    const textbox = getByRole('combobox');
+    textbox.focus();
+    fireEvent.change(textbox, { target: { value: 'foobar' } });
+    const noResults = screen.getByTestId('no-results');
+    expect(noResults).to.contain.html('<em>No suggestions available.</em>');
+  });
+
+  it('should correctly set aria-activedescendant prop on option selection', () => {
+    const { getAllByRole } = render(<ComboBox options={MOCK_OPTIONS} />);
+    const textbox = screen.getByRole('combobox');
+    textbox.focus();
     fireEvent.keyDown(textbox, { key: 'ArrowDown' }); // open the popup
     fireEvent.keyDown(textbox, { key: 'ArrowDown' }); // focus the first option
     const options = getAllByRole('option');
     expect(textbox).to.have.attribute('aria-activedescendant', options[0].getAttribute('id'));
+  });
 
+  it('should select the correct option', () => {
+    const { getByRole } = render(<ComboBox options={MOCK_OPTIONS} />);
+    const textbox = getByRole('combobox');
+    textbox.focus();
+    fireEvent.keyDown(textbox, { key: 'ArrowDown' }); // open the popup
+    fireEvent.keyDown(textbox, { key: 'ArrowDown' }); // focus the first option
     fireEvent.keyDown(textbox, { key: 'Enter' }); // select the first option
-    expect(handleSubmit.callCount).to.equal(0);
-    expect(handleChange.callCount).to.equal(1);
+    expect(textbox).to.have.attribute('value', 'foo');
   });
 });
